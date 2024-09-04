df4ada734ef55e6faec1957172f9d914c49864a1
rude-dm <damasonrudy30@gmail.com>
2024-09-04 09:09:18 +0800

    First Commit

---
+++ assignment-html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Personal Information</h1>
+
+<?php
+$fullname = "Rudy Damason";
+$address = "123 Main St.";
+$contact = "09876543211";
+$hobbies = "Biting Nails, swimming, and hiking.";
+$socialMedia = "Facebook, Twitter, Instagram,Tiktok";
+
+echo "<b>Full Name:</b> " . $fullname . "<br>";
+echo "<b>Address:</b> " . $address . "<br>";
+echo "<b>Contact:</b> " . $contact . "<br>";
+echo "<b>Hobbies:</b> " . $hobbies . "<br>";
+echo "<b>Social Media:</b> " . $socialMedia;
+?>
+
+</body>
+</html>
